593bda57b07f376b4b6217c152a8761b16f24709
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2023-01-06 14:46:58 +0530

    Update main2.swift

--- day6/main2.swift
+++ day6/main2.swift
@@ -1,55 +1,67 @@
 import Foundation
 
-class UserException {
-    // create an enum with error values
-    enum MyExceptions: Error {
-        case dividedByZero
-        case invalidIndex
-        case invalidArrayIndex
+enum MyExceptions: Error {
+    case dividedByZero
+    case invalidIndex
+    case invalidArrayIndex
+}
+
+// create an enum with error values
+
+// create a throwing function using throws keyword
+func division(numerator: Int, denominator: Int) throws {
+
+  // throw error if divide by 0
+    if denominator == 0 {
+      throw MyExceptions.dividedByZero
     }
+    
+    else {
+      let result = numerator / denominator
+      print(result)
+    }
+}
 
-    // create a throwing function using throws keyword
-    static func division(numerator: Int, denominator: Int) throws {
+func indexFinder(str: String, num: Int) throws {
+    if num < 0 || num >= str.count {
+        throw MyExceptions.invalidIndex
+    }
+    else {
+        let indx = str.index(str.startIndex, offsetBy: num)
+        print(str[indx])
+    }
+}
 
-      // throw error if divide by 0
-        if denominator == 0 {
-          throw MyExceptions.dividedByZero
+
+class Exc {
+    
+    init () {
+        
+        // call throwing function from do block
+        do {
+            try division(numerator: 10, denominator: 10)
+            print("Valid Division")
+            
+            do {
+                try indexFinder(str: "sajeev", num: 2)
+                print("Valid index")
+            } catch MyExceptions.invalidIndex {
+                print("Index is Invalid")
+                throw MyExceptions.invalidIndex
+            }
+
+        } catch MyExceptions.dividedByZero {
+            print("Invalid Division Error")
+        }
+        catch MyExceptions.invalidIndex {
+            print("Rethrowed Exception")
+        }
+        catch {
+            print("Exception handled")
         }
         
-        else {
-          let result = numerator / denominator
-          print(result)
-        }
     }
-
-    static func indexFinder(str: String, num: Int) throws {
-        if num < 0 || num >= str.count {
-            throw MyExceptions.invalidIndex
-        }
-        else {
-            let indx = str.index(str.startIndex, offsetBy: num)
-            print(str[indx])
-        }
-    }
-
-}
-
-// call throwing function from do block
-do {
-    try UserException.division(numerator: 10, denominator: 10)
-    print("Valid Division")
     
-    do {
-        try UserException.indexFinder(str: "sajeev", num: 2)
-        print("Valid index")
-    } catch UserException.MyExceptions.invalidIndex {
-        print("Index is Invalid")
-        throw UserException.MyExceptions.invalidIndex
-    }
+}
 
-} catch UserException.MyExceptions.dividedByZero {
-    print("Invalid Division Error")
-}
-catch UserException.MyExceptions.invalidIndex {
-    print("Rethrowed Exception")
-}
+var t = Exc()
